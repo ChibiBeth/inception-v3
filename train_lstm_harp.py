930461743f07110f583bce18aead32a3ede623f0
Bethania <betha.chibi@gmail.com>
2023-02-02 00:29:59 -0300

    Ultimos cambios a train lstm intentando juntar todo

---
+++ train_lstm_harp.py
@@ -0,0 +1,246 @@
+from keras.layers import Dense, Flatten, Dropout, ZeroPadding3D, Input
+from keras.layers.recurrent import LSTM
+from keras.models import Model, Sequential, load_model
+from keras.optimizers import Adam
+from collections import deque
+import sys
+from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping, CSVLogger
+import time
+import os.path
+import csv
+import numpy as np
+import random
+import glob
+import sys
+import operator
+import threading
+from keras.preprocessing import image as Img
+from keras.applications.inception_v3 import InceptionV3, preprocess_input
+from tqdm import tqdm
+from keras.utils import to_categorical
+import os 
+import cv2 
+
+class DataSet():
+
+    def __init__(self, seq_length=40, class_limit=None, image_shape=(224, 224, 3)):
+        self.seq_length = seq_length
+        self.class_limit = class_limit
+        self.sequence_path = os.path.join('data', 'sequences')
+        self.max_frames = 300  # max number of frames a video can have for us to use it
+        self.data = self.get_data()
+        self.classes = self.get_classes()
+        self.data = self.clean_data()
+        self.image_shape = image_shape
+
+    @staticmethod
+    def get_data():
+        with open(os.path.join('data', 'data_file.csv'), 'r') as fin:
+            reader = csv.reader(fin)
+            #print(list(reader))
+            data = list(reader)
+            print('Retornando data para clase dataset')
+            print(data)
+        return data
+
+    def clean_data(self):
+        data_clean = []
+        print('Data antes del clean')
+        print(self.data)
+        for item in self.data:
+            if int(item[3]) >= self.seq_length and int(item[3]) <= self.max_frames \
+                    and item[1] in self.classes:
+                data_clean.append(item)
+
+        return data_clean
+
+    def get_classes(self):
+        classes = []
+        for item in self.data:
+            if item[1] not in classes:
+                classes.append(item[1])
+        classes = sorted(classes)
+        if self.class_limit is not None:
+            return classes[:self.class_limit]
+        else:
+            return classes
+
+    def get_class_one_hot(self, class_str):
+        # Encode it first.
+        label_encoded = self.classes.index(class_str)
+        # Now one-hot it.
+        label_hot = to_categorical(label_encoded, len(self.classes))
+        assert len(label_hot) == len(self.classes)
+        return label_hot
+
+    def split_train_test(self):
+        train = []
+        test = []
+        for item in self.data:
+            if item[0] == 'train':
+                train.append(item)
+            else:
+                test.append(item)
+        return train, test
+
+    def get_all_sequences_in_memory(self, train_test, data_type):
+        train, test = self.split_train_test()
+        data = train if train_test == 'train' else test
+
+        print("Loading %d samples into memory for %sing." % (len(data), train_test))
+
+        X, y = [], []
+        for row in data:
+            sequence = self.get_extracted_sequence(data_type, row)
+            if sequence is None:
+                print("Can't find sequence. Did you generate them?")
+                raise
+            X.append(sequence)
+            y.append(self.get_class_one_hot(row[1]))
+        return np.array(X), np.array(y)
+
+    def get_extracted_sequence(self, data_type, sample):
+        filename = sample[2]
+        print('Aca hay seqlength')
+        print(self.seq_length)
+        path = os.path.join(self.sequence_path, filename + '-' + str(self.seq_length) + \
+            '-' + data_type + '.npy')
+        if os.path.isfile(path):
+            return np.load(path)
+        else:
+            return None
+
+    def get_frames_by_filename(self, filename, data_type):
+        sample = None
+        for row in self.data:
+            if row[2] == filename:
+                sample = row
+                break
+        if sample is None:
+            raise ValueError("Couldn't find sample: %s" % filename)
+        sequence = self.get_extracted_sequence(data_type, sample)
+        if sequence is None:
+            raise ValueError("Can't find sequence. Did you generate them?")
+        return sequence
+
+    @staticmethod
+    def get_frames_for_sample(sample):
+        """Given a sample row from the data file, get all the corresponding frame
+        filenames."""
+        path = os.path.join('data', sample[0], sample[1])
+        print('Path donde busca frames')
+        print(path)
+        filename = sample[2]
+        images = sorted(glob.glob(os.path.join(path, filename + '*jpg')))
+        return images
+
+    @staticmethod
+    def rescale_list(input_list, size):
+        print('Longitud de input list')
+        print(len(input_list))
+        assert len(input_list)>= size
+        skip = len(input_list) // size
+        output = [input_list[i] for i in range(0, len(input_list), skip)]
+        return output[:size]
+
+checkpointer = ModelCheckpoint(
+    filepath=os.path.join('data', 'checkpoints','lstm-features' + '.{epoch:03d}-{val_loss:.3f}.hdf5'),
+    verbose=1,
+    save_best_only=True)
+
+# Helper: TensorBoard
+tb = TensorBoard(log_dir=os.path.join('data', 'logs', 'lstm'))
+
+# Helper: Stop when we stop learning.
+early_stopper = EarlyStopping(patience=10)
+
+# Helper: Save results.
+timestamp = time.time()
+csv_logger = CSVLogger(os.path.join('data', 'logs', 'lstm' + '-' + 'training-' + \
+    str(timestamp) + '.log'))
+
+# Get the data and process it.
+data = DataSet(
+    seq_length=40,
+    class_limit=70
+)
+#listt=[]
+#listt2=[]
+X, y = data.get_all_sequences_in_memory('train', 'features')
+X_test, y_test = data.get_all_sequences_in_memory('test', 'features')
+
+model = Sequential()
+model.add(LSTM(2048, return_sequences=False,input_shape=(40,2048),dropout=0.5))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(data.classes), activation='softmax'))
+optimizer = Adam(lr=1e-5, decay=1e-6)
+model.compile(loss='categorical_crossentropy', optimizer=optimizer,
+                    metrics=['accuracy','top_k_categorical_accuracy'])
+print(model.summary())
+
+model.fit(
+    X,
+    y,
+    batch_size=32,
+    validation_data=(X_test, y_test),
+    verbose=1,
+    callbacks=[tb, early_stopper, csv_logger,checkpointer],
+    epochs=100)
+
+def rescale_list(input_list, size):
+    assert len(input_list) >= size
+    skip = len(input_list) // size
+    output = [input_list[i] for i in range(0, len(input_list), skip)]
+    return output[:size]
+classes = glob.glob("data/train/*")
+classes = [classes[i].split('/')[2] for i in range(len(classes))]
+classes = sorted(classes)
+
+image_name = 'soltero_hugo.avi'
+cam = cv2.VideoCapture(image_name) 
+currentframe = 0
+  
+frames=[]
+while(True): 
+    ret,frame = cam.read() 
+    if ret: 
+        # if video is still left continue creating images 
+        name = 'testFinal/frame'+'9' +"frame_no"+ str(currentframe) + '.jpg'
+        cv2.imwrite(name, frame) 
+        frames.append(name)  
+        currentframe += 1
+    else: 
+        break
+cam.release() 
+cv2.destroyAllWindows()
+rescaled_list = rescale_list(frames,40)
+
+base_model = InceptionV3(
+    weights='imagenet',
+    include_top=True
+)
+# We'll extract features at the final pool layer.
+inception_model = Model(
+    inputs=base_model.input,
+    outputs=base_model.get_layer('avg_pool').output
+)
+sequence = []
+for image in rescaled_list:
+        img = Img.load_img(image, target_size=(299, 299))
+        x = Img.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        features = inception_model.predict(x)
+        sequence.append(features[0])
+
+sequence = np.array([sequence])
+prediction = model.predict(sequence)
+maxm = prediction[0][0]
+maxid = 0
+for i in range(len(prediction[0])):
+      if(maxm<prediction[0][i]):
+            maxm = prediction[0][i]
+            maxid = i
+#print(frames)
+print(image_name,' ------- ',classes[maxid])
